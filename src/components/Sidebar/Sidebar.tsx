63190692512e84d95439a37b43de4a3817d3b933
Vendula Krajíčková <vendysacek@seznam.cz>
2024-06-06 21:20:15 +0200

    add project structure

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,34 @@
+import './Sidebar.css';
+
+export const Sidebar = () => {
+
+  return (
+    <aside className="sidebar">
+      <nav className="menu">
+        <div className="menu__item menu__logo">
+          <a href="#" className="menu__link">
+            <img className="menu__icon" src="assets/icons/kiwi.svg" />
+          </a>
+        </div>
+        <div className="menu__item">
+          <a href="#" className="menu__link">
+            <img className="menu__icon" src="assets/icons/house.svg" alt="Domů" />
+            <span className="menu__text">Domů</span>
+          </a>
+        </div>
+        <div className="menu__item">
+          <a href="#" className="menu__link">
+            <img className="menu__icon" src="assets/icons/bookmark.svg" alt="Záložky" />
+            <span className="menu__text">Záložky</span>
+          </a>
+        </div>
+        <div className="menu__item">
+          <a href="#" className="menu__link">
+            <img className="menu__icon" src="assets/icons/heart.svg" alt="Lajky" />
+            <span className="menu__text">Lajky</span>
+          </a>
+        </div>
+      </nav>
+    </aside>
+  )
+}
