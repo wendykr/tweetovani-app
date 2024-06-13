98b03874ccedea8824507e7a7b7050d83570dd5e
Vendula Krajíčková <vendysacek@seznam.cz>
2024-06-13 09:29:21 +0200

    #2 - create SidebarRight and put Search into

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import { Search } from './components/Search/Search';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Outlet } from 'react-router-dom';
+import { SidebarRight } from './components/SidebarRight/SidebarRight';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <h1 className="page-title">Tweetování</h1>
         <Outlet />
       </main>
-      <Search />
+      <SidebarRight />
     </div>
   );
 }
---
+++ src/components/SidebarRight/SidebarRight.tsx
@@ -0,0 +1,10 @@
+import { Search } from '../Search/Search';
+import './SidebarRight.css';
+
+export const SidebarRight = () => {
+  return (
+    <aside className="sidebar-right">
+      <Search />
+    </aside>
+  );
+};
