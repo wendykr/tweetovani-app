28c8702f0b526add1e9c1bba26baa6dac5baa5a0
Vendula Krajíčková <vendysacek@seznam.cz>
2024-07-26 11:28:34 +0200

    #1 - feat: create ErrorPage comp

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import './ErrorPage.css';
+
+export const ErrorPage = () => {
+  return (
+    <div className="errorPage">
+      <h3>Hmm... tato stránka neexistuje</h3>
+      <p>
+        ... přejdi na hlavní stránku nebo využij nabídky vlevé části obrazovky.
+      </p>
+      <Link className="errorPage__button" to="/">
+        Domů
+      </Link>
+    </div>
+  );
+};
