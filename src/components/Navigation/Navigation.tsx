d648a142a435322c677d8ee13ff111db01a49cf5
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-22 09:10:23 +0200

    #19 - feat: create post button

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,11 +1,18 @@
 import { Menu } from '../Menu/Menu';
 import { UserCard } from '../UserCard/UserCard';
 import './Navigation.css';
+import { FaFeatherAlt } from 'react-icons/fa';
 
 export const Navigation = () => {
   return (
     <nav className="navigation">
-      <Menu />
+      <div className="navigation__head">
+        <Menu />
+        <div className="navigation__icon--button">
+          <FaFeatherAlt className="navigation__icon" />
+        </div>
+        <button className="buttonPost">Tweetni to</button>
+      </div>
       <UserCard />
     </nav>
   );
