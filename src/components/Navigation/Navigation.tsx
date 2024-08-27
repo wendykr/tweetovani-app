ad80cfe08864938a9f1e6e3e2578f87dc353b1ff
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-27 14:25:39 +0200

    #19 - feat: create PopUpContext

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,17 +1,22 @@
+import { usePopUp } from '../../context/PopUpContext';
 import { Menu } from '../Menu/Menu';
 import { UserCard } from '../UserCard/UserCard';
 import './Navigation.css';
 import { FaFeatherAlt } from 'react-icons/fa';
 
 export const Navigation = () => {
+  const { onClickPopUp } = usePopUp();
+
   return (
     <nav className="navigation">
       <div className="navigation__head">
         <Menu />
-        <div className="navigation__icon--button">
+        <div className="navigation__icon--button" onClick={onClickPopUp}>
           <FaFeatherAlt className="navigation__icon" />
         </div>
-        <button className="buttonPost">Tweetni to</button>
+        <button className="buttonPost" onClick={onClickPopUp}>
+          Tweetni to
+        </button>
       </div>
       <UserCard />
     </nav>
